8eea6e8fe09940539691601e9675473917c917c3
cas <caswenting@gmail.com>
2026-02-26 10:48:29 +0100

    Initial commit

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -34,10 +34,10 @@
             // button1
             // 
             button1.Location = new Point(148, 118);
-            button1.Name = "button1";
+            button1.Name = "Knopjuh";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "Knopjuh";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
