e401dcd697f17773e67af8564f1d078af760a623
cas <caswenting@gmail.com>
2026-03-12 11:19:59 +0100

    small changes before starting the project

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -17,34 +17,49 @@
             btnShuffle = new Button();
             lblCard = new Label();
             lblDeckCount = new Label();
-
-            //btnDeal
+            SuspendLayout();
+            // 
+            // btnDeal
+            // 
             btnDeal.Location = new Point(50, 200);
+            btnDeal.Name = "btnDeal";
             btnDeal.Size = new Size(100, 34);
+            btnDeal.TabIndex = 0;
             btnDeal.Text = "Deal new card";
             btnDeal.Click += btnDeal_Click;
-
-            //btnShuffle
+            // 
+            // btnShuffle
+            // 
             btnShuffle.Location = new Point(170, 200);
+            btnShuffle.Name = "btnShuffle";
             btnShuffle.Size = new Size(100, 34);
+            btnShuffle.TabIndex = 1;
             btnShuffle.Text = "Shuffle";
             btnShuffle.Click += btnShuffle_Click;
-
-            //lblCard
-            lblCard.Location = new Point(50, 80);
-            lblCard.Size = new Size(120, 80);
-            lblCard.Font = new Font("Segoe UI", 28, FontStyle.Bold);
+            // 
+            // lblCard
+            // 
             lblCard.BorderStyle = BorderStyle.FixedSingle;
-            lblCard.TextAlign = ContentAlignment.MiddleCenter;
+            lblCard.Font = new Font("Segoe UI", 28F, FontStyle.Bold);
+            lblCard.Location = new Point(50, 80);
+            lblCard.Name = "lblCard";
+            lblCard.Size = new Size(120, 80);
+            lblCard.TabIndex = 2;
             lblCard.Text = "?";
-
-            //lblDeckCount
-            lblDeckCount.Location = new Point(50, 170);
+            lblCard.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblDeckCount
+            // 
             lblDeckCount.AutoSize = true;
-            lblDeckCount.Font = new Font("Segoe UI", 10);
+            lblDeckCount.Font = new Font("Segoe UI", 10F);
+            lblDeckCount.Location = new Point(50, 170);
+            lblDeckCount.Name = "lblDeckCount";
+            lblDeckCount.Size = new Size(109, 23);
+            lblDeckCount.TabIndex = 3;
             lblDeckCount.Text = "Cards left: 52";
-
-            //Form1
+            // 
+            // Form1
+            // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(400, 300);
---
+++ WinFormsApp1/Hand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsApp1
+{
+    internal class Hand
+    {
+
+    }
+}
